3f3abd88e06bd0ef9044db4bc00e7424d9ed9bf6
Momoko Kono <momoko.kono@fixstars.com>
2023-02-28 08:01:36 +0900

    manually set payload size of device payload size

--- src/arvcameratest.c
+++ src/arvcameratest.c
@@ -544,6 +544,7 @@ main (int argc, char **argv)
 		if (error == NULL) arv_camera_set_gain (camera, arv_option_gain, &error);
 
 		if (arv_camera_is_uv_device (camera)) {
+
 			if (error == NULL)
                                 arv_camera_uv_set_usb_mode (camera, usb_mode);
 			if (error == NULL && arv_option_bandwidth_limit >= 0)
@@ -585,6 +586,15 @@ main (int argc, char **argv)
 			if (error == NULL && arv_camera_is_gain_available (camera, NULL))
 				gain = arv_camera_get_gain (camera, &error);
 			if (error == NULL) payload = arv_camera_get_payload (camera, &error);
+			// =============================================================
+			// since payload size is 0, i just set it manually for debug
+			gint image_size = 1920 * 1080 * 2;
+			gint descriptor_size = (96 - 32 + 56 + 72)/8;
+			payload = image_size + descriptor_size;
+			printf("=========================================================\n");
+			printf("arvcameratest.c\tSince the payload size on GenICam features is 0, manually set to %i\n", payload);
+			printf("=========================================================\n");
+			// =============================================================
 
 			if (arv_camera_is_uv_device (camera)) {
 				if (arv_camera_uv_is_bandwidth_control_available (camera, NULL)) {
@@ -646,6 +656,10 @@ main (int argc, char **argv)
                     }
 
 		    if (ARV_IS_STREAM (stream)) {
+				// =============================================================
+				// camera is U3V
+				printf("arvcameratest.c ARV_IS_STREAM (stream)\n");
+				// =============================================================
 			    if (ARV_IS_GV_STREAM (stream)) {
 				    if (arv_option_auto_socket_buffer)
 					    g_object_set (stream,
@@ -666,7 +680,9 @@ main (int argc, char **argv)
 						  "packet-timeout", (unsigned) arv_option_packet_timeout * 1000,
 						  "frame-retention", (unsigned) arv_option_frame_retention * 1000,
 						  NULL);
-			    }
+			    }else{
+					printf("                !ARV_IS_GV_STREAM (stream)\n");
+				}
 
 			    for (i = 0; i < 50; i++)
 				    arv_stream_push_buffer (stream, arv_buffer_new (payload, NULL));
